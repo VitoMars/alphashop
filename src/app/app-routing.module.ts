a81124732582d52197d2a2afe40c986cbc365b03
VitoMars <vito95v@gmail.com>
2022-08-06 13:53:08 +0200

    Inserimento RouteGuard ed icone alla navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RouteGuardService } from 'src/services/route-guard.service';
 import { ArticoliComponent } from './articoli/articoli.component';
 import { ErrorComponent } from './error/error.component';
 import { LoginComponent } from './login/login.component';
@@ -9,8 +10,16 @@ import { WelcomeComponent } from './welcome/welcome.component';
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'welcome/:userid', component: WelcomeComponent },
-  { path: 'articoli', component: ArticoliComponent },
+  {
+    path: 'welcome/:userid',
+    component: WelcomeComponent,
+    canActivate: [RouteGuardService],
+  },
+  {
+    path: 'articoli',
+    component: ArticoliComponent,
+    canActivate: [RouteGuardService],
+  },
   { path: 'logout', component: LogoutComponent },
   { path: '**', component: ErrorComponent },
 ];
